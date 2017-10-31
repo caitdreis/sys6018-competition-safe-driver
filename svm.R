7a4eddca4d1af529a252b7d3e09fdf086ce9231b
sally-gao <sg2zv@virginia.edu>
2017-10-31 00:22:09 -0400

    order cat variables by significance

--- svm.R
+++ svm.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(e1071)
+library(car)
 
 train <- read_csv("train.csv")
 
@@ -12,7 +13,7 @@ train %>%
 
 # EXPLORATORY ANALYSIS: CATEGORICAL VARIABLES ==============================================================
 
-categorical_variables <- c("ps_ind_01", "ps_ind_02_cat", "ps_ind_02_cat", "ps_ind_03", "ps_ind_04_cat", "ps_ind_05_cat",
+categorical_variables <- c("ps_ind_01", "ps_ind_02_cat", "ps_ind_03", "ps_ind_04_cat", "ps_ind_05_cat",
                            "ps_ind_06_bin", "ps_ind_07_bin", "ps_ind_08_bin", "ps_ind_09_bin", "ps_ind_10_bin", "ps_ind_11_bin",
                            "ps_ind_12_bin", "ps_ind_13_bin", "ps_ind_14", "ps_ind_15",  "ps_ind_16_bin",  "ps_ind_17_bin",
                            "ps_ind_18_bin", "ps_car_01_cat",  "ps_car_02_cat",  "ps_car_03_cat",  "ps_car_04_cat",
@@ -29,7 +30,8 @@ chisq_tests <- function (categories, df, target) {
   
   for (category in categories) {
     names <- c(names, category)
-    p_value <- c(p_value, chisq.test(table(df[[category]], df[[target]]))$p.value)
+    
+    p_value <- c(p_value, chisq.test(df[[category]], df[[target]])$p.value)
   }
   
   return(data.frame(names, p_value))
@@ -38,33 +40,25 @@ chisq_tests <- function (categories, df, target) {
 # Get p-value for contingeny table chi-square test for every category:
 train_chisq <- chisq_tests(categorical_variables, train, "target")
 
-train_chisq2 <- train_chisq %>% 
+train_chisq <- train_chisq %>% 
   mutate(
-    significant_.05 = ifelse(p_value < .05, "Yes", "No"),
-    significant_.1 = ifelse(p_value < .1, "Yes", "No"))
+    significant_.05 = ifelse(p_value < .05, "Yes", "No")) %>% 
+  arrange(p_value)
 
 train_chisq
 
 # Significant categorical variables:
+sig_cat_vars <- train_chisq %>%
+  filter (significant_.05=="Yes") %>% 
+  select(names) %>% 
+  unlist %>% 
+  as.vector
 
+sig_cat_vars
 
 # EXPLORATORY ANALYSIS: CONTINUOUS VARIABLES ==============================================================
 
-target.factor <- as.factor(train$target)
-
-train %>% 
-  ggplot() + geom_point(mapping = aes(x=target.factor, y=ps_reg_01), position="jitter", alpha = .08)
-
-qplot(factor(train$ps_reg_01), geom="bar", fill=factor(train$target))
-qplot(train$ps_reg_01, geom="histogram", bins = 20, fill=factor(train$target))
-
-train %>% 
-  ggplot() + geom_histogram(mapping = aes(ps_reg_01, fill = target.factor), bins = 20)
-
-testglm <- glm(train$target ~ train$ps_reg_01, family=binomial("logit"))
-summary(testglm)
-
-testglm2 <- glm(target ~ ps_reg_01 +
+testglm <- glm(target ~ ps_reg_01 +
                   ps_reg_02 +
                   ps_reg_03 +
                   ps_car_12 +
@@ -75,15 +69,17 @@ testglm2 <- glm(target ~ ps_reg_01 +
                   ps_calc_02 +
                   ps_calc_03, data=train, family=binomial("logit"))
 
-vif(testglm2)
+vif(testglm)
 # No multicollinearity issues.
 
-summary(testglm2)
+summary(testglm)
 # All variables significant except ps_calc_01, ps_calc_02, ps_calc_03.
 
 # BUILD LINEAR AND SVM MODEL ===================================================================
 
 # The SVM function only works if we build a dataframe containing no unused variables.
+
+# (Using only sig_cat_vars[1:10])
 
 svm_train <- data.frame(target = factor(train$target),
                         ps_reg_01 = train$ps_reg_01,
@@ -93,21 +89,39 @@ svm_train <- data.frame(target = factor(train$target),
                         ps_car_13 = train$ps_car_13,
                         ps_car_14 = train$ps_car_14,
                         ps_car_15 = train$ps_car_15,
-                        ps_ind_01 = factor(train$ps_ind_01),
-                        ps_ind_02_cat = factor(train$ps_ind_02_cat),
-                        ps_ind_03 = factor(train$ps_ind_03),
-                        ps_ind_04_cat = factor(train$ps_ind_04_cat),
                         ps_ind_05_cat = factor(train$ps_ind_05_cat),
-                        ps_ind_14 = factor(train$ps_ind_14),
+                        ps_ind_06_bin = factor(train$ps_ind_06_bin),
+                        ps_ind_07_bin = factor(train$ps_ind_07_bin),
+                        ps_ind_17_bin = factor(train$ps_ind_17_bin),
                         ps_car_01_cat = factor(train$ps_car_01_cat),
                         ps_car_04_cat = factor(train$ps_car_04_cat),
+                        ps_car_03_cat = factor(train$ps_car_03_cat),
                         ps_car_06_cat = factor(train$ps_car_06_cat),
                         ps_car_07_cat = factor(train$ps_car_07_cat),
-                        ps_car_09_cat = factor(train$ps_car_09_cat)
+                        ps_car_11_cat = factor(train$ps_car_11_cat)
                         )
 
-quick_svm <- svm(target ~ ., svm_train)
+quick_svm <- svm(target ~ ., svm_train, probability=TRUE)
 
 # kernel = "radial"
 # kernel = "linear"
 # kernel = "polynomial"
+
+# testy stuff ######################
+
+svm_train_test <- svm_train %>% 
+  sample_frac(size = .02)
+
+Sys.time()
+quick_svm_test <- svm(target ~ ., svm_train_test, probability=TRUE)
+Sys.time()
+
+# Training error
+pred <- predict(quick_svm_test, svm_train_test, probability=TRUE)
+
+pred.df <- data.frame(attr(pred, "probabilities"))
+
+normalized.gini.index(as.numeric(svm_train_test$target), pred.df$X1)
+# 0.9829379
+
+
