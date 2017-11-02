1c8049e1ef90917081eac013613f86bcefff1fc1
sally-gao <sg2zv@virginia.edu>
2017-11-02 16:23:37 -0400

    added gini.R

---
+++ gini.R
@@ -0,0 +1,40 @@
+#' Calculates unnormalized Gini index from ground truth and predicted probabilities.
+#' @param ground.truth Ground-truth scalar values (e.g., 0 and 1)
+#' @param predicted.probabilities Predicted probabilities for the items listed in ground.truth
+#' @return Unnormalized Gini index.
+unnormalized.gini.index = function(ground.truth, predicted.probabilities) {
+  
+  if (length(ground.truth) !=  length(predicted.probabilities))
+  {
+    stop("Actual and Predicted need to be equal lengths!")
+  }
+  
+  # arrange data into table with columns of index, predicted values, and actual values
+  gini.table = data.frame(index = c(1:length(ground.truth)), predicted.probabilities, ground.truth)
+  
+  # sort rows in decreasing order of the predicted values, breaking ties according to the index
+  gini.table = gini.table[order(-gini.table$predicted.probabilities, gini.table$index), ]
+  
+  # get the per-row increment for positives accumulated by the model 
+  num.ground.truth.positivies = sum(gini.table$ground.truth)
+  model.percentage.positives.accumulated = gini.table$ground.truth / num.ground.truth.positivies
+  
+  # get the per-row increment for positives accumulated by a random guess
+  random.guess.percentage.positives.accumulated = 1 / nrow(gini.table)
+  
+  # calculate gini index
+  gini.sum = cumsum(model.percentage.positives.accumulated - random.guess.percentage.positives.accumulated)
+  gini.index = sum(gini.sum) / nrow(gini.table) 
+  return(gini.index)
+}
+
+#' Calculates normalized Gini index from ground truth and predicted probabilities.
+#' @param ground.truth Ground-truth scalar values (e.g., 0 and 1)
+#' @param predicted.probabilities Predicted probabilities for the items listed in ground.truth
+#' @return Normalized Gini index, accounting for theoretical optimal.
+normalized.gini.index = function(ground.truth, predicted.probabilities) {
+  
+  model.gini.index = unnormalized.gini.index(ground.truth, predicted.probabilities)
+  optimal.gini.index = unnormalized.gini.index(ground.truth, ground.truth)
+  return(model.gini.index / optimal.gini.index)
+}
